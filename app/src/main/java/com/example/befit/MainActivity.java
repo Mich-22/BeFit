3cfce1f7877320bfd52365c67d74a4d45e415d09
Michelle Lopez <michelle.lopezsilva@hotmail.com>
2022-04-21 23:11:39 -0500

    Cambio de pantalla principal a login

--- app/src/main/java/com/example/befit/MainActivity.java
+++ app/src/main/java/com/example/befit/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.befit;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goToLogin();
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 1000);
+
     }
 }
