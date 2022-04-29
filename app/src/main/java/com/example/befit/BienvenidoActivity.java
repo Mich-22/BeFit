3f14322377b647860ba4a8cfd1317ca5407ca50b
Michelle Lopez <michelle.lopezsilva@hotmail.com>
2022-04-29 16:36:18 -0500

    Login de facebook funcionando y redireccionando a pantalla de bienvenida

--- app/src/main/java/com/example/befit/BienvenidoActivity.java
+++ app/src/main/java/com/example/befit/BienvenidoActivity.java
@@ -1,17 +1,14 @@
 package com.example.befit;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.facebook.login.widget.LoginButton;
 
 public class BienvenidoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bienvenidofb);
+        setContentView(R.layout.activity_bienvenido);
     }
 }
--- app/src/main/java/com/example/befit/LoginActivity.java
+++ app/src/main/java/com/example/befit/LoginActivity.java
@@ -3,17 +3,23 @@ package com.example.befit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -37,20 +43,75 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 String asd = loginResult.toString();
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                String holaaa = response.toString();
+                                try {
+                                    String name = object.getString("name");
+                                    String email = object.getString("email");
+                                    Toast.makeText(LoginActivity.this, "Bienvenid@ " + name, Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, BienvenidoActivity.class);
+                                    startActivity(intent);
+                                } catch (Exception ex){
+
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+
             }
 
             @Override
             public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Cancelado", Toast.LENGTH_SHORT).show();
                 // App code
             }
 
             @Override
             public void onError(FacebookException exception) {
+                String error = exception.toString();
+                Toast.makeText(LoginActivity.this, error, Toast.LENGTH_LONG).show();
                 // App code
             }
         });
-    }
 
+        /*callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        String asd = loginResult.toString();
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        String asd = "loginResult.toString();";
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        String asd = exception.toString();
+                        // App code
+                    }
+                });*/
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
     public void mostrarFormularioRegistro(View view) {
         // Do something in response to button click
         Intent intent = new Intent(this, RegistroActivity.class);
