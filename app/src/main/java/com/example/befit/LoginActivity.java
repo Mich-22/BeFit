8f6ac2edba468c5d2b15344723b04e4b50f01669
Michelle Lopez <michelle.lopezsilva@hotmail.com>
2022-04-29 14:34:50 -0500

    Login con facebook

--- app/src/main/java/com/example/befit/LoginActivity.java
+++ app/src/main/java/com/example/befit/LoginActivity.java
@@ -4,14 +4,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
+    private static final String EMAIL = "email";
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String asd = loginResult.toString();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 
     public void mostrarFormularioRegistro(View view) {
--- app/src/main/java/com/example/befit/MainActivity.java
+++ app/src/main/java/com/example/befit/MainActivity.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
     final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);*/
         setContentView(R.layout.activity_main);
         goToLogin();
+
     }
 
     public void goToLogin() {
