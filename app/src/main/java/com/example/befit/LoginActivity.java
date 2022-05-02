45ec493682da9a1b2734a3837a1daf944fea4e17
Michelle Lopez <michelle.lopezsilva@hotmail.com>
2022-05-01 22:59:02 -0500

    Usuarios se guardan en la base de datos Firestore con Facebook y boton registrarse

--- app/src/main/java/com/example/befit/LoginActivity.java
+++ app/src/main/java/com/example/befit/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.befit;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -15,31 +19,69 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    //region Facebook properties
     private static final String EMAIL = "email";
     CallbackManager callbackManager;
     LoginButton loginButton;
+    //endregion
 
+    //region Google properties
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    SignInButton signInButton;
+    Button signOutButton;
+    TextView statusTextView;
+    GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final  int RC_SIGN_IN = 9001;
+    private static final int REQ_ONE_TAP = 2;
+    private boolean showOneTapUI = true;
+    private FirebaseAuth mAuth;
+    //endregion
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        db = FirebaseFirestore.getInstance();
+
+        //region Facebook init
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
-        // If you are using in a fragment, call loginButton.setFragment(this);
-
-        // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
             @Override
             public void onSuccess(LoginResult loginResult) {
                 String asd = loginResult.toString();
@@ -55,6 +97,7 @@ public class LoginActivity extends AppCompatActivity {
                                 try {
                                     String name = object.getString("name");
                                     String email = object.getString("email");
+                                    guardarUsusarioEnFirestore(name, email);
                                     Toast.makeText(LoginActivity.this, "Bienvenid@ " + name, Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
                                     startActivity(intent);
@@ -84,37 +127,150 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        /*callbackManager = CallbackManager.Factory.create();
+        //endregion
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        String asd = loginResult.toString();
-                        // App code
-                    }
+        //region Google init
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+                .requestEmail()
+                .build();
 
-                    @Override
-                    public void onCancel() {
-                        String asd = "loginResult.toString();";
-                        // App code
-                    }
+        mGoogleApiClient =new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-                    @Override
-                    public void onError(FacebookException exception) {
-                        String asd = exception.toString();
-                        // App code
-                    }
-                });*/
+        statusTextView = (TextView) findViewById(R.id.status_textview);
+        signInButton = (SignInButton) findViewById(R.id.btngmail);
+        signInButton.setOnClickListener(this);
+
+
+
+        signOutButton = (Button) findViewById(R.id.sign_Out_Button);
+        signOutButton.setOnClickListener(this);
+
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId("306683492977-40hj10ntarvpgvon2e4bmc21r7f5spsb.apps.googleusercontent.com")
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
+        //endregion
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode,data);
+
+        //region Facebook result
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        //endregion
+
+        //region Google result
+        if (requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSingInResult(result);
+        }
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        Log.d(TAG, "Got ID token.");
+                    }
+                } catch (ApiException e) {
+                    // ...
+                }
+                break;
+        }
+        //endregion
     }
+
+    //region Google methods
+
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btngmail:
+                signIn();
+
+
+            case R.id.sign_Out_Button:
+                signOut();
+                break;
+        }
+    }
+
+    public void  handleSingInResult(GoogleSignInResult result){
+        Log.d(TAG, "HandleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()){
+            GoogleSignInAccount acct = result.getSignInAccount();
+            statusTextView.setText("Hello, "   + acct.getDisplayName());
+        } else{
+
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult){
+        Log.d(TAG,  "onConnectionFailed" +  connectionResult);
+    }
+
+
+    public void signOut(){
+        /*Auth.GoogleSignInApi.signOut (mGoogleApiClient).setResultCallback(new ResultCallback<Status>(){
+            @Override
+                    public void onResult(@NonNull Status status){
+                    statusTextView.setText("Signed out");
+            }
+        });*/
+    }
+    //endregion
+
     public void mostrarFormularioRegistro(View view) {
         // Do something in response to button click
         Intent intent = new Intent(this, RegistroActivity.class);
         startActivity(intent);
     }
+
+    public void guardarUsusarioEnFirestore(String nombre, String email){
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("nombre", nombre);
+        userData.put("email", email);
+        userData.put("apellido", "");
+        userData.put("domicilio", "");
+        userData.put("telefono", "");
+        userData.put("password", "");
+
+
+        // Add a new document with a generated ID
+        db.collection("usuarios")
+                .document(email.toString())
+                .set(userData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(LoginActivity.this, "Bienvenid@ " + nombre, Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Ha ocurrido un error", Toast.LENGTH_LONG).show();
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/befit/RegistroActivity.java
+++ app/src/main/java/com/example/befit/RegistroActivity.java
@@ -2,18 +2,77 @@ package com.example.befit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistroActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    TextView txtNombre;
+    TextView txtApellido;
+    TextView txtDomicilio;
+    TextView  txtTelefono;
+    TextView txtEmail;
+    TextView  txtPassword;
+    private static final String TAG = "RegistroActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        db = FirebaseFirestore.getInstance();
+
+        txtNombre = (TextView) findViewById(R.id.txtNombre);
+        txtApellido = (TextView) findViewById(R.id.txtApellido);
+        txtDomicilio = (TextView) findViewById(R.id.txtDomicilio);
+        txtTelefono = (TextView) findViewById(R.id.txtTelefono);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
+
+
     }
     public void mostrarPantallaPrincipal(View view) {
+        // Create a new user with a first and last name
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("nombre", txtNombre.getText().toString());
+        userData.put("apellido", txtApellido.getText().toString());
+        userData.put("domicilio", txtDomicilio.getText().toString());
+        userData.put("telefono", txtTelefono.getText().toString());
+        userData.put("email", txtEmail.getText().toString());
+        userData.put("password", txtPassword.getText().toString());
+
+
+        // Add a new document with a generated ID
+        db.collection("usuarios")
+            .document(txtEmail.getText().toString())
+            .set(userData)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(RegistroActivity.this, "Bienvenid@ " + txtNombre.getText().toString(), Toast.LENGTH_LONG).show();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(RegistroActivity.this, "Ha ocurrido un error", Toast.LENGTH_LONG).show();
+                    Log.w(TAG, "Error adding document", e);
+                }
+            });
+
         Intent intent = new Intent( RegistroActivity.this, WelcomeActivity.class);
         startActivity(intent);
     }
